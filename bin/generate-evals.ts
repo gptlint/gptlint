f0c40fa32a373be69e54ae929d6ac354e7698168
Travis Fischer <fisch0920@gmail.com>
2024-03-31 18:15:46 -0500

    🏝

--- bin/generate-evals.ts
+++ bin/generate-evals.ts
@@ -33,6 +33,7 @@ async function main() {
   const { args, linterConfig: config } = await resolveLinterCLIConfig(
     process.argv,
     {
+      name: 'generate-evals',
       cwd,
       linterConfigDefaults: {
         llmOptions: {
