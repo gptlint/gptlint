2af9c0e292e31cd4903c5c6563a4b28dd5ccadc7
Travis Fischer <fisch0920@gmail.com>
2024-04-03 01:34:58 -0500

    docs: improve readme; add how-it-works.md; profit

--- src/rule-violations.ts
+++ src/rule-violations.ts
@@ -27,28 +27,33 @@ export const ruleViolationSchema = z.object({
     .string()
     .optional()
     .describe('The name of the RULE which this `codeSnippet` violates.'),
+
   codeSnippet: z
     .string()
     .describe(
       'The offending code snippet which fails to conform to the given RULE. CODE SNIPPETS MUST BE SHORT and should include an ellipsis "..." if they would be more than 10 lines of code.'
     ),
+
   codeSnippetSource: z
     .enum(['examples', 'source', 'unknown'])
     .optional()
     .describe(
       'Where the `codeSnippet` comes from. If it comes from the RULE examples, then use "examples". If it comes from the SOURCE, then use "source".'
     ),
+
   reasoning: z
     .string()
     .optional()
     .describe(
       'An explanation of why this code snippet VIOLATES the RULE. Think step-by-step when describing your reasoning.'
     ),
+
   violation: z
     .boolean()
     .describe(
       'Whether or not this `codeSnippet` violates the RULE. If the `codeSnippet` does VIOLATE the RULE, then `violation` should be `true`. If the `codeSnippet` conforms to the RULE correctly or does not appear in the SOURCE, then `violation` should be `false`.'
     ),
+
   confidence: z
     .enum(['low', 'medium', 'high'])
     .describe('Your confidence that the `codeSnippet` VIOLATES the RULE.')
