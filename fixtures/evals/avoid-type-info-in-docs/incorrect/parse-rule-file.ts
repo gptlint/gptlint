e6d6bcb1d87f5c682c9ccae880ab34b069627383
Travis Fischer <fisch0920@gmail.com>
2024-04-06 02:29:21 -0500

    🌕

---
+++ fixtures/evals/avoid-type-info-in-docs/incorrect/parse-rule-file.ts
@@ -0,0 +1,35 @@
+import type * as types from './types.js'
+import {
+  findAllBetween,
+  findAllHeadingNodes,
+  parseMarkdownAST,
+  parseRuleNode
+} from './markdown-utils.js'
+import { assert } from './utils.js'
+
+/**
+ * Parses a rule definition markdown file and returns the result.
+ * 
+ * Takes in `content` which is a string and `filePath` which is a string.
+ */
+export async function parseRuleFile({
+  content,
+  filePath
+}: {
+  content: string
+  filePath: string
+}): Promise<types.Rule> {
+  const ast = parseMarkdownAST(content)
+  const h1RuleNodes = findAllHeadingNodes(ast, { depth: 1 })
+
+  assert(
+    h1RuleNodes.length === 1,
+    `Rule file must contain a single h1 header: ${filePath}`
+  )
+
+  const headingRuleNode = h1RuleNodes[0]!
+  const bodyRuleNodes = findAllBetween(ast, headingRuleNode)
+
+  const rule = parseRuleNode({ headingRuleNode, bodyRuleNodes, filePath })
+  return rule
+}
