51005be5a6758a43686e478f0b678ee4adcb284e
Travis Fischer <fisch0920@gmail.com>
2024-04-09 22:45:25 -0500

    🐳

--- docs/pages/_meta.ts
+++ docs/pages/_meta.ts
@@ -15,11 +15,20 @@ export default {
     type: 'separator',
     title: 'Links'
   },
+  github: {
+    title: 'GitHub',
+    type: 'page',
+    href: 'https://github.com/gptlint/gptlint'
+  },
   contact: {
     title: 'Contact ↗',
     type: 'page',
     href: 'mailto:gptlint@teamduality.dev',
     newWindow: true
+  },
+  'github-sidebar': {
+    title: 'GitHub',
+    href: 'https://github.com/gptlint/gptlint'
   },
   'contact-sidebar': {
     title: 'Contact ↗',
