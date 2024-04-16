0feb3e46c158b2f7462d827f1e4b432417c8a729
Travis Fischer <fisch0920@gmail.com>
2024-04-16 03:15:25 -0500

    🌰

--- docs/pages/rules/_meta.ts
+++ docs/pages/rules/_meta.ts
@@ -1,39 +1,43 @@
 export default {
   index: 'Overview',
-  'consistent-identifier-casing': {
-    title: 'consistent-identifier-casing',
-    display: 'hidden'
-  },
   'always-handle-promises': {
     title: 'always-handle-promises',
     display: 'hidden'
   },
-  'no-hardcoded-secrets': {
-    title: 'no-hardcoded-secrets',
+  'consistent-identifier-casing': {
+    title: 'consistent-identifier-casing',
     display: 'hidden'
   },
   'avoid-type-info-in-docs': {
     title: 'avoid-type-info-in-docs',
     display: 'hidden'
   },
+  'no-hardcoded-secrets': {
+    title: 'no-hardcoded-secrets',
+    display: 'hidden'
+  },
   'liberal-accept-strict-produce': {
     title: 'liberal-accept-strict-produce',
-    display: 'hidden'
-  },
-  'prefer-loose-array-bounds-checks-in-loops': {
-    title: 'prefer-loose-array-bounds-checks-in-loops',
-    display: 'hidden'
-  },
-  'prefer-array-at-negative-indexing': {
-    title: 'prefer-array-at-negative-indexing',
     display: 'hidden'
   },
   'prefer-types-always-valid-states': {
     title: 'prefer-types-always-valid-states',
     display: 'hidden'
   },
+  'prefer-array-at-negative-indexing': {
+    title: 'prefer-array-at-negative-indexing',
+    display: 'hidden'
+  },
+  'prefer-loose-array-bounds-checks-in-loops': {
+    title: 'prefer-loose-array-bounds-checks-in-loops',
+    display: 'hidden'
+  },
   'react-avoid-class-components': {
     title: 'react-avoid-class-components',
+    display: 'hidden'
+  },
+  'soc2-no-leak-user-data': {
+    title: 'soc2-no-leak-user-data',
     display: 'hidden'
   },
   'semantic-variable-names': {
@@ -42,10 +46,6 @@ export default {
   },
   'use-correct-english': {
     title: 'use-correct-english',
-    display: 'hidden'
-  },
-  'soc2-no-leak-user-data': {
-    title: 'soc2-no-leak-user-data',
     display: 'hidden'
   }
 }
